152adffcc1c0086a4ad698b8b298443c20ca5086
Stardust <yuliya.boyadjan@gmail.com>
2018-07-27 08:55:27 -0400

    exercises

---
+++ fibonacci.js
@@ -0,0 +1,9 @@
+function calcFib (n) {
+    var fib = [0, 1];
+    for (var i = 2; i < n; i ++) {
+      var fibNum = fib[i - 2] + fib[i - 1];
+      fib.push(fibNum);
+    }
+    return fib;
+}
+console.log(calcFib(10));
