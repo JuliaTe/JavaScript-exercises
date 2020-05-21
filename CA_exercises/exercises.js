7f25be50e035321839c99103e5944b3513e3caab
Julia Thea <yuliya.boyadjan@gmail.com>
2020-05-21 14:54:13 -0500

    stuff

--- CA_exercises/exercises.js
+++ CA_exercises/exercises.js
@@ -671,6 +671,20 @@ function displayAirplane() {
 
 displayAirplane();
 
+let Airplane = {};
+
+Airplane.availableAirplanes = [
+{
+  name: 'AeroJet',
+  fuelCapacity: 800
+ }, 
+ {name: 'SkyJet',
+  fuelCapacity: 500
+ }
+];
+
+export default Airplane;
+
 
 
 
