2edbe6b1dcffb09d749f05d07db6b65348d62c15
Julia Thea <yuliya.boyadjan@gmail.com>
2020-05-23 16:07:34 -0500

    stuff

--- CA_exercises/exercises.js
+++ CA_exercises/exercises.js
@@ -734,7 +734,17 @@ Airplane.availableAirplanes = [
   const orderPromise = orderSunglasses();
   console.log(orderPromise);
 
-
+  console.log("This is the first line of code in app.js.");
+  // Keep the line above as the first line of code
+  // Write your code here:
+  const usingSTO = () => {
+    console.log("Hello Jules! You are doing a great job!")
+  }
+  
+  setTimeout(usingSTO, 60)
+  
+  // Keep the line below as the last line of code:
+  console.log("This is the last line of code in app.js.");
 
 
 
