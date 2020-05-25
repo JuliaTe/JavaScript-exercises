f92f5064a8eec7f9fdcb038d236386cbd46e4b75
Julia Thea <yuliya.boyadjan@gmail.com>
2020-05-24 19:27:31 -0500

    stuff

--- CA_exercises/exercises.js
+++ CA_exercises/exercises.js
@@ -767,7 +767,21 @@ const handleFailure = (rejectReason) => {
 checkInventory(order).then(handleSuccess, handleFailure)
 
 
+const {checkInventory} = require('./library.js');
 
+const order = [['sunglasses', 1], ['bags', 2]];
+
+const handleSuccess = (resolvedValue) => {
+  console.log(resolvedValue);
+};
+
+const handleFailure = (rejectReason) => {
+  console.log(rejectReason);
+};
+
+// Write your code below:
+
+checkInventory(order).then(handleSuccess).catch(handleFailure);
 
 
 
