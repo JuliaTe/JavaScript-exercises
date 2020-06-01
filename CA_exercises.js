eea492b2aa9fa6994c4fa555e11f00fa71cfa44d
Julia Thea <yuliya.boyadjan@gmail.com>
2020-06-01 15:18:49 -0500

    stuff

--- CA_exercises.js
+++ CA_exercises.js
@@ -934,6 +934,42 @@ async function readFiles() {
 readFiles()
 
 
+/* ASYNC module */
+
+function withConstructor(num){
+  return new Promise((resolve, reject) => {
+    if (num === 0){
+      resolve('zero');
+    } else {
+      resolve('not zero');
+    }
+  })
+}
+
+withConstructor(1)
+  .then((resolveValue) => {
+  console.log(` withConstructor(0) returned a promise which resolved to: ${resolveValue}.`);
+})
+
+// Write your code below:
+
+async function withAsync(num){
+  if (num === 0){
+    return 'zero';
+  }
+  else {
+    return 'not zero';
+  }
+}
+
+// Leave this commented out until step 3:
+
+withAsync(00)
+  .then((resolveValue) => {
+  console.log(` withAsync(100) returned a promise which resolved to: ${resolveValue}.`);
+})
+
+
 
 
 
