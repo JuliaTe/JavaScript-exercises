3a72b8d4dcc236ee702104ce09599a5f1ddb77a6
Julia Thea <yuliya.boyadjan@gmail.com>
2020-06-02 09:21:19 -0500

    stuff

--- CA_exercises.js
+++ CA_exercises.js
@@ -1022,6 +1022,19 @@ async function getBeans() {
 
 getBeans();
 
+const {shopForBeans, soakTheBeans, cookTheBeans} = require('./library.js');
+
+// Write your code below:
+
+async function makeBeans() {
+  let type = await shopForBeans();
+  let isSoft = await soakTheBeans(type);
+  let dinner = await cookTheBeans(isSoft);
+  console.log(dinner);
+}
+
+makeBeans();
+
 
 
 
