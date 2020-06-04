78710c0e03a149345edee2e509058df7489351df
Julia Thea <yuliya.boyadjan@gmail.com>
2020-06-04 13:41:21 -0500

    stuff

--- CA_exercises.js
+++ CA_exercises.js
@@ -1225,6 +1225,48 @@ xhr.open('POST', url);
 xhr.send(data);
 
 
+// Information to reach API
+const apiKey = 'dc9060c3045b4d5381299e0b8ab31455';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  const xhr = new XMLHttpRequest;
+  xhr.responseType = 'json';
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+  renderResponse(xhr.response);
+}
+  }
+  xhr.open('POST', url);
+  xhr.setRequestHeader('Content-type', 'application/json');
+xhr.setRequestHeader('apikey', apiKey);
+xhr.send(data);
+}
+
+
+
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
+
+
+
 
 
 
