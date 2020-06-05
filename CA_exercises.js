6f12bff5c629f9f8d5999557463573b9cb19e460
Julia Thea <yuliya.boyadjan@gmail.com>
2020-06-05 12:12:02 -0500

    stuff

--- CA_exercises.js
+++ CA_exercises.js
@@ -1281,6 +1281,46 @@ networkError => {
 });
 
 
+const url = 'https://api.datamuse.com/words';
+const queryParams = '?sl=';
+// Information to reach API
+
+
+// Selects page elements
+const inputField = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
+
+// AJAX function
+const getSuggestions = () => {
+  const wordQuery = inputField.value;
+  const endpoint = `${url}${queryParams}${wordQuery}`;
+  fetch(endpoint, {cache: 'no-cache'})
+  .then(response => {
+    if(response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  },
+    (networkError => {
+    console.log(networkError.message)
+  }))
+
+};
+
+// Clears previous results and display results to webpage
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  getSuggestions();
+};
+
+submit.addEventListener('click', displaySuggestions);
+
+
+
 
 
 
