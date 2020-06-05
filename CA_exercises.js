db29774b99be5b9f3b44587fe3129f0807141a6d
Julia Thea <yuliya.boyadjan@gmail.com>
2020-06-05 14:15:05 -0500

    stuff

--- CA_exercises.js
+++ CA_exercises.js
@@ -1449,6 +1449,32 @@ submit.addEventListener('click', displaySuggestions);
 
 
 
+let availableAirplanes = [{
+  name: 'AeroJet',
+  fuelCapacity: 800,
+  availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
+ }, 
+ {name: 'SkyJet',
+  fuelCapacity: 500,
+  availableStaff: ['pilots', 'flightAttendants']
+ }];
+ 
+ let flightRequirements = {
+   requiredStaff: 4,
+ };
+ 
+ function meetsStaffRequirements(availableStaff, requiredStaff) {
+   if(availableStaff.length >= requiredStaff) {
+     return true
+   }
+   else {
+     return false
+   }
+ }
+ 
+ export { availableAirplanes, flightRequirements, meetsStaffRequirements};
+
+
 
 
 
