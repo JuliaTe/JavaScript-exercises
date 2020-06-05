b4cf99dc24512c3f734e6b49f372a13f686bd894
Julia Thea <yuliya.boyadjan@gmail.com>
2020-06-05 12:37:13 -0500

    stuff

--- CA_exercises.js
+++ CA_exercises.js
@@ -1375,6 +1375,42 @@ fetch('https://api-to-call.com/endpoint', {
 })
 
 
+// Information to reach API
+const apiKey = 'dc9060c3045b4d5381299e0b8ab31455';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination:urlToShorten});
+  fetch(url, {
+    method: 'POST',
+    headers: {
+  'Content-type': 'application/json',
+  'apikey': apiKey
+},
+body: data
+  });
+}
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild)
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
+
+
+
 
 
 
