019c4174541c2e8ab70047cf8545df99505dbbff
Julia Thea <yuliya.boyadjan@gmail.com>
2020-06-06 19:01:21 -0500

    stuff

--- CA_exercises.js
+++ CA_exercises.js
@@ -1525,7 +1525,24 @@ export let availableAirplanes = [{
    }
  }
  
+ import { availableAirplanes, flightRequirements, meetsStaffRequirements, meetsSpeedRangeRequirements } from './airplane';
+
+ function displayStaffStatus() {
+   availableAirplanes.forEach(function(element) {
+    console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff) );
+   });
+ }
  
+ function displaySpeedRangeStatus() {
+   availableAirplanes.forEach(function(element) {
+    console.log(element.name + ' meets speed range requirements: ' + meetsSpeedRangeRequirements(element.maxSpeed, element.minSpeed, flightRequirements.requiredSpeedRange));
+   });
+ }
+ 
+ 
+ displayStaffStatus();
+ displaySpeedRangeStatus();
+  
 
 
 
