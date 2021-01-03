36f4dde4a3596ff2a788751c1d81ee37e32f0680
Julia Thea <yuliya.boyadjan@gmail.com>
2021-01-03 12:40:47 -0600

    finished exercise

---
+++ totalFruits.js
@@ -0,0 +1,16 @@
+function convertScoreToGrade(score) {
+  // your code here
+  if (score) {
+    return 'A';
+  } else if (score) {
+    return 'B';
+  } else if (score) {
+    return 'C';
+  } else (score) {
+    return 'D';
+  } else if (score) {
+    return 'E';
+  } else {
+      return 'INVALID SCORE';
+  }
+}
