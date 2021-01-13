08833a25bacf7f218f6bba6faf8a47b70271b529
Julia Thea <yuliya.boyadjan@gmail.com>
2021-01-13 08:43:50 -0600

    finished exercise

---
+++ Hack_Reactor_exercises/more_help.js
@@ -0,0 +1,50 @@
+function  moreHelp(schedule, employees) {
+
+}
+
+// Test function
+
+// Test cases
+
+
+// var schedule = [
+//   {day: 'Monday', time: 'slot 1'},
+//   {day: 'Tuesday', time: 'slot 3'},
+//   {day: 'Thursday', time: 'slot 1'},
+//   {day: 'Thursday', time: 'slot 4'},
+//   {day: 'Friday', time: 'slot 2'}
+// ]
+
+// var employees = [
+//   {
+//     availableDays:['Monday', 'Tuesday', 'Friday'], 
+//     times: ['slot 1', 'slot 2'], 
+//     name: 'David'},
+//   {
+//     availableDays: ['Tuesday', 'Thursday'], 
+      //  times: ['slot 3', 'slot 4'],
+//     name: 'Jessica'
+//   },
+//    {
+//     availableDays: ['Monday', 'Friday'], 
+      //  times: ['slot 3', 'slot 4'],
+//     name: 'Adam'
+//   },
+//   {
+//     availableDays:['Sunday', 'Thursday', 'Friday'],
+//     times: ['slot 1', 'slot 2'],
+//     name: 'Chelsea'
+//   }
+// ]
+
+let expected1 = [
+  {
+    days: [],
+    time: [],
+    name: ""
+  }
+]
+
+
+
+// moreHelp(schedule, employees);
