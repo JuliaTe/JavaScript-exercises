36c8911de653fd0ef664b6f036bbf23f1cdecc80
Julia Thea <yuliya.boyadjan@gmail.com>
2021-01-19 09:36:35 -0600

    finished exercise

--- flatten_array.js
+++ flatten_array.js
@@ -1,24 +1,39 @@
-function flattenArray(input) {
-  var result = [];
+// First approach
+// function flattenArray(input) {
+//   var result = [];
   
-  var innerFunction = function(array) {
-    if (!Array.isArray(array)) {
-      result.push(array);
-      return;
-    }
+//   var innerFunction = function(array) {
+//     if (!Array.isArray(array)) {
+//       result.push(array);
+//       return;
+//     }
 
-    array.forEach(function(item) {
-      innerFunction(item);
-    })
-  };
+//     array.forEach(function(item) {
+//       innerFunction(item);
+//     })
+//   };
 
-  innerFunction(input);
+//   innerFunction(input);
 
+//   return result;
+// };
+
+// Second approach
+var flattenArray = function(input) {
+  var result = [];
+
+  if (!Array.isArray(input)) {
+    return input;
+  }
+
+  input.forEach(function(item) {
+    result = result.concat(flattenArray(item));
+  })
   return result;
 };
 
-// flattenArray(4); // [4]
-// flattenArray([4]); // [4]
+flattenArray(4); // [4]
+flattenArray([4]); // [4]
  // [1, 2, 3, 4, 5]
-console.log(flattenArray([1, 2, [3, [[4]]], 5]));
-// flattenArray([[[4]]]); // [4]
+flattenArray([1, 2, [3, [[4]]], 5]);
+console.log(flattenArray([[[4]]])); // [4]
